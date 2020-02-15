a8d03f89f38831d7ef6afece668011fdbbf8b140
Felipe Kunzler <felipe.kunzler@sap.com>
2020-02-15 18:11:57 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.16"
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "com.modulenavigator"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2019.3.3"
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
